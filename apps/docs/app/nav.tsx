85468553d1ef30b8979b34a6cf81db9fe881e97c
Omar Ibrahim <omaribrahim@uchicago.edu>
2025-08-29 15:22:33 -0500

    docs: add active link detection to nav

--- apps/docs/app/nav.tsx
+++ apps/docs/app/nav.tsx
@@ -2,8 +2,33 @@
 import NetLogoUserManualSVG from '@repo/ui/assets/brands/NetLogoUserManual.svg';
 import Navbar from '@repo/ui/layout/navbar/Navbar';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { isWindowDefined } from '@repo/ui/utils/client';
+import { useLayoutEffect, useState } from 'react';
 
 export default function ClientNavbar() {
+  const [links, setLinks] = useState(navbarLinks);
+  const pathname = usePathname();
+
+  if (isWindowDefined()) {
+    useLayoutEffect(() => {
+      setLinks((prevLinks) =>
+        prevLinks.map((link) => ({
+          ...link,
+          active: isLinkParentActive(link),
+          children: link.children?.map((child) => ({
+            ...child,
+            active: isSublinkActive(child.href),
+          })),
+        }))
+      );
+    }, [pathname]);
+  }
+
   return (
     <Navbar.Client
       id="main-navbar"
@@ -11,12 +36,13 @@ export default function ClientNavbar() {
       brandHref="/"
     >
       <Navbar.LinksContainer>
-        {navbarLinks.map((link) => (
+        {links.map((link) => (
           <Navbar.ItemClient
             key={link.title}
             title={link.title}
             href={link.href}
             columns={link.columns}
+            active={link.active}
           >
             {link.children &&
               link.children.map((child) => (
@@ -24,19 +50,44 @@ export default function ClientNavbar() {
                   key={child.title}
                   title={child.title}
                   href={child.href}
+                  active={child.active}
                 />
               ))}
           </Navbar.ItemClient>
         ))}
       </Navbar.LinksContainer>
       <Navbar.ActionsContainer>
-        <Navbar.Action title="GitHub" href="https://github.com/NetLogo" />
+        <Navbar.Action
+          icon={<FontAwesomeIcon icon={faGithub} />}
+          href="https://github.com/NetLogo"
+        />
+        <Navbar.Action
+          icon={<FontAwesomeIcon icon={faFilePdf} />}
+          href="/NetLogo_User_Manual.pdf"
+        />
       </Navbar.ActionsContainer>
     </Navbar.Client>
   );
 }
 
-const navbarLinks = [
+function isSublinkActive(href: string | undefined) {
+  if (typeof window === 'undefined') return false;
+  if (!href) return false;
+  try {
+    const currentPath = window.location.pathname;
+    return new URL(href, window.location.href).pathname === currentPath;
+  } catch {
+    return false;
+  }
+}
+
+function isLinkParentActive(link: NavbarLink) {
+  if (typeof window === 'undefined') return false;
+  const currentPath = window.location.pathname;
+  return link.children?.some((child) => isSublinkActive(child.href)) || false;
+}
+
+const navbarLinks: NavbarLink[] = [
   {
     title: 'Home',
     href: './',
@@ -94,3 +145,11 @@ const navbarLinks = [
     href: 'faq.html',
   },
 ];
+
+interface NavbarLink {
+  title: string;
+  href: string;
+  columns?: number;
+  children?: Omit<NavbarLink, 'children'>[];
+  active?: boolean;
+}
