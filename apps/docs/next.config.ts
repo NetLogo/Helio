5f63eaee7704be0505d4e570bd94b0d0925ad5dc
Omar Ibrahim <omaribrahim@uchicago.edu>
2025-09-15 14:24:00 -0500

    docs: add base path to next urls

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -1,12 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images: { unoptimized: true },
+  basePath: `/${process.env['PRODUCT_VERSION']}`,
+  publicRuntimeConfig: {
+    basePath: `/${process.env['PRODUCT_VERSION']}`,
   },
 };
 
