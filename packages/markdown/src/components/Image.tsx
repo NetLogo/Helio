2b0c538ec1b5416fa307e51a33d9b60c88c2ee0b
Omar Ibrahim <omaribrahim@uchicago.edu>
2025-09-19 13:30:34 -0500

    @repo/markdown: drop the default screenshot class on images

--- packages/markdown/src/components/Image.tsx
+++ packages/markdown/src/components/Image.tsx
@@ -2,7 +2,7 @@ import ImageTagElement from '@repo/ui/HOC/Image';
 import { Components } from 'react-markdown';
 
 const Image: Components['img'] = ({ node, ...props }) => {
-  return <ImageTagElement {...props} className="screenshot" alt={props.alt || 'Screenshot'} />;
+  return <ImageTagElement {...props} alt={props.alt || 'Screenshot'} />;
 };
 
 export default Image;
