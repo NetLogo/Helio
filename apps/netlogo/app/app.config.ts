17ee3c901532e30ed1f386a9971b8238d9eaecda
danielwong0115 <daniellwong442@gmail.com>
2026-01-30 11:46:27 -0600

    Add in Get NetLogo and Community sections on home page

--- apps/netlogo/app/app.config.ts
+++ apps/netlogo/app/app.config.ts
@@ -6,38 +6,7 @@ export default defineAppConfig({
     },
 
     container: {
-      base: "max-w-[150ch] mx-auto px-0 mx-auto px-[var(--space-xl)] lg:px-0",
-    },
-
-    breadcrumb: {
-      slots: {
-        list: "[&_li]:m-0! p-0",
-      },
-    },
-
-    contentNavigation: {
-      slots: {
-        list: "mx-0 px-2 lg:mt-[var(--block-top)]",
-        listWithChildren: "px-0",
-      },
-    },
-    contentToc: {
-      slots: {
-        root: "px-0! w-full -mx-0",
-        trailing: "hidden",
-        title:
-          "w-full px-[var(--space-lg)] py-2 bg-[var(--secondary-heading-background-color)] text-[var(--secondary-heading-text-color)] w-full",
-      },
-    },
-    pageAside: {
-      slots: {
-        root: "pt-0",
-      },
-    },
-    blogPost: {
-      slots: {
-        footer: "p-4 sm:p-6",
-      },
+      base: "max-w-[150ch] px-4 lg:px-12 mx-auto",
     },
   },
 });
