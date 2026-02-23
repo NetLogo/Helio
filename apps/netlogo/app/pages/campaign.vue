03d948fbe0443989b1bcc9fcac04428340bb56eb
danielwong0115 <daniellwong442@gmail.com>
2026-02-23 13:30:23 -0600

    Implement campaign page

---
+++ apps/netlogo/app/pages/campaign.vue
@@ -0,0 +1,24 @@
+<template>
+  <UContainer>
+    <AboutSection v-if="campaignData" :about-data="campaignData" />
+  </UContainer>
+</template>
+
+<script setup lang="ts">
+import NetLogoAPI from "~/utils/api";
+
+useHead({
+  title: "NetLogo Center Campaign",
+  titleTemplate: "%s",
+  meta: [
+    { name: "description", content: "About The NetLogo Center Campaign" },
+  ],
+});
+
+const config = useRuntimeConfig();
+
+const { data: campaignData } = await useAsyncData("campaign-data", async () => {
+  const api = new NetLogoAPI(config.public.backendUrl as string);
+  return await api.getCampaignContent();
+});
+</script>
