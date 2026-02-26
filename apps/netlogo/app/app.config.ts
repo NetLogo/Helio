390c2dfb38f8ef8ff338c910a2443a0022cc64d9
danielwong0115 <daniellwong442@gmail.com>
2026-02-26 15:06:34 -0600

    Update download form with NuxtUI Forms

--- apps/netlogo/app/app.config.ts
+++ apps/netlogo/app/app.config.ts
@@ -6,7 +6,27 @@ export default defineAppConfig({
     },
 
     container: {
-      base: "max-w-[150ch] px-4 lg:px-12 mx-auto",
+      base: "max-w-[150ch] px-4 lg:px-24 mx-auto",
+    },
+
+    input: {
+      slots: {
+        root: "w-full",
+        base: "py-2!",
+      },
+    },
+
+    formField: {
+      slots: {
+        label: "font-bold text-lg",
+      },
+      variants: {
+        orientation: {
+          horizontal: {
+            root: "grid grid-cols-12 gap-4 items-center [&>:first-child]:col-span-3 [&>:nth-child(2)]:col-span-9 [&>:nth-child(2)]:w-full",
+          },
+        },
+      },
     },
   },
 });
--- apps/netlogo/app/components/downloads/DownloadSection.vue
+++ apps/netlogo/app/components/downloads/DownloadSection.vue
@@ -1,46 +1,44 @@
 <template>
-  <UContainer>
-    <div class="container pt-3">
-      <h1 class="font-semibold text-start text-4xl">Download NetLogo for {{ devOs }}</h1>
+  <div class="container pt-3">
+    <h1 class="font-semibold text-start text-4xl">Download NetLogo for {{ devOs }}</h1>
 
-      <div class="grid grid-cols-1 lg:grid-cols-2 gap-10 items-start mt-6">
-        <div>
-          <DownloadForm :versions="downloadData" :dev-os="devOs" />
-        </div>
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-10 items-start mt-6">
+      <div>
+        <DownloadForm :versions="downloadData" :dev-os="devOs" />
+      </div>
 
-        <div class="text-start text-lg ps-5 mt-4">
-          <p>
-            Most computers can run NetLogo (see
-            <a
-              target="_blank"
-              class="underline text-blue-600"
-              href="https://docs.netlogo.org/requirements.html"
-              >system requirements</a
-            >). If you would like to run NetLogo on a Chromebook or in a web browser, use
-            <a class="underline text-blue-600" target="_blank" href="http://netlogoweb.org/"
-              >NetLogo Web</a
-            >.
-          </p>
-          <p class="mt-3">Multiple versions of NetLogo can be installed on the same computer.</p>
-          <p class="mt-3">
-            For help using old models with new versions see the
-            <a
-              target="_blank"
-              class="underline text-blue-600"
-              href="https://docs.netlogo.org/transition.html"
-              >Transition Guide</a
-            >.
-          </p>
+      <div class="text-start text-lg ps-5 mt-4">
+        <p>
+          Most computers can run NetLogo (see
+          <a
+            target="_blank"
+            class="underline text-blue-600"
+            href="https://docs.netlogo.org/requirements.html"
+            >system requirements</a
+          >). If you would like to run NetLogo on a Chromebook or in a web browser, use
+          <a class="underline text-blue-600" target="_blank" href="http://netlogoweb.org/"
+            >NetLogo Web</a
+          >.
+        </p>
+        <p class="mt-3">Multiple versions of NetLogo can be installed on the same computer.</p>
+        <p class="mt-3">
+          For help using old models with new versions see the
+          <a
+            target="_blank"
+            class="underline text-blue-600"
+            href="https://docs.netlogo.org/transition.html"
+            >Transition Guide</a
+          >.
+        </p>
 
-          <img
-            :src="netlogoIcon"
-            class="mt-0 mb-0 max-w-[65%] block mx-auto"
-            alt="Download NetLogo Turtle"
-          />
-        </div>
+        <img
+          :src="netlogoIcon"
+          class="mt-0 mb-0 max-w-[65%] block mx-auto"
+          alt="Download NetLogo Turtle"
+        />
       </div>
     </div>
-  </UContainer>
+  </div>
 </template>
 
 <script setup lang="ts">
