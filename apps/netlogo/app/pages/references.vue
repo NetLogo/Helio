89b99bfe3a34e12ca0bfa2d535f0d08cc2c3875e
danielwong0115 <daniellwong442@gmail.com>
2026-03-20 03:52:12 -0700

    Initial implementation of references page without year anchor links

---
+++ apps/netlogo/app/pages/references.vue
@@ -0,0 +1,13 @@
+<template>
+  <UContainer>
+    <ReferencesSection />
+  </UContainer>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: "NetLogo References",
+  titleTemplate: "%s",
+  meta: [{ name: "description", content: "List of publications which use NetLogo" }],
+});
+</script>
