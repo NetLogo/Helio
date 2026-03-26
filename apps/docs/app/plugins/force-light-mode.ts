5bc4be5ee89566fc3554f47f55ca9590887f37de
Omar Ibrahim <omaribrahim@uchicago.edu>
2026-03-26 14:36:13 -0500

    chore: update @nuxtjs/color-mode force light mode settings

--- apps/docs/app/plugins/force-light-mode.ts
+++ apps/docs/app/plugins/force-light-mode.ts
@@ -1,7 +1,11 @@
 export default defineNuxtPlugin((nuxtApp) => {
   const colorMode = useColorMode();
-  nuxtApp.hook("app:mounted", () => {
-    colorMode.preference = "light";
-    colorMode.value = "light";
+  nuxtApp.hook('app:mounted', () => {
+    colorMode.forced = true;
+    // @ts-expect-error - preference may or may not exist depending
+    // on how @nuxtjs/color-mode was installed
+    colorMode.preference = 'light';
+    // @ts-expect-error - see above
+    colorMode.value = 'light';
   });
 });
