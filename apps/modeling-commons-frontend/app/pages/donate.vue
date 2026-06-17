ebc8ddb22946ce2fbac68ee353695acf61aff857
Omar Ibrahim <omaribrahim@uchicago.edu>
2026-06-16 22:07:22 -0500

    Minor(modeling-commons): Add 'make a donation' button

--- apps/modeling-commons-frontend/app/pages/donate.vue
+++ apps/modeling-commons-frontend/app/pages/donate.vue
@@ -1,6 +1,6 @@
 <template>
   <UPage>
-    <UPageHero :title="hero.title">
+    <UPageHero :title="hero.title" :links="hero.links" class="relative">
       <template #title>
         <span v-for="(line, index) in hero.title.split('\n')" :key="index" class="block">
           {{ line }}
@@ -16,10 +16,21 @@
 </template>
 
 <script lang="ts" setup>
+import type { PageHeroProps } from "#ui/types";
 const hero = {
   title: "Help us build a more\nscientifically-literate world",
   description: [
     "Together, we’re building a future where complex models aren’t locked away in labs, but accessible to everyone",
   ],
-};
+  links: [
+    {
+      label: "Make a Donation",
+      icon: "lucide:heart",
+      to: "https://give.northwestern.edu/campaigns/37603/donations/new?a=9727882&designation_id=N3004411",
+      external: true,
+      variant: "solid",
+    },
+  ],
+} satisfies Omit<PageHeroProps, "description"> & { description: string[] };
+
 </script>
