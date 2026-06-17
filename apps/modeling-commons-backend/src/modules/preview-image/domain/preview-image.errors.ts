74ceac46ea2e510aa8a916d10524fb1d7b1be131
Omar Ibrahim <omaribrahim@uchicago.edu>
2026-06-16 22:16:28 -0500

    Minor(modeling-commons): Use more informative error message for failed preview image generation

--- apps/modeling-commons-backend/src/modules/preview-image/domain/preview-image.errors.ts
+++ apps/modeling-commons-backend/src/modules/preview-image/domain/preview-image.errors.ts
@@ -1,19 +1,19 @@
 import { ProviderErrorException } from '#src/shared/exceptions/exceptions.ts';
 
 export class ModelPreviewServiceError extends ProviderErrorException {
-  constructor(netlogoFileKey: string, cause?: Error) {
-    super(`Preview image for NetLogo file ${netlogoFileKey} could not be generated`, cause);
+  constructor(message: string = 'Model preview generation failed') {
+    super(message);
   }
 }
 
 export class ModelPreviewTimeoutError extends ModelPreviewServiceError {
-  constructor(netlogoFileKey: string) {
-    super(netlogoFileKey, new Error(`Preview image generation timed out after 30 seconds`));
+  constructor() {
+    super(`Preview image generation timed out after 30 seconds`);
   }
 }
 
 export class ModelPreviewTooLargeError extends ModelPreviewServiceError {
-  constructor(netlogoFileKey: string) {
-    super(netlogoFileKey, new Error(`Preview image exceeded size limit of 5MB`));
+  constructor() {
+    super(`Preview image exceeded size limit of 5MB`);
   }
 }
