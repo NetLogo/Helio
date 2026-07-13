f554db00110b99ab7605ebc91e423fbf66ab5d5e
Omar Ibrahim <omaribrahim@uchicago.edu>
2026-07-13 14:31:49 -0500

    Feat(docs): Rewrite absolute URLs to relative URLs

--- apps/docs/nuxt.config.static.ts
+++ apps/docs/nuxt.config.static.ts
@@ -25,6 +25,36 @@ const staticOverrides: Parameters<DefineNuxtConfig>[0] = {
   gtag: { enabled: false },
   ogImage: { enabled: false },
   linkChecker: { enabled: false },
+
+  nitro: {
+    hooks: {
+      'prerender:generate'(route) {
+        if (!route.fileName?.endsWith('.html') || !route.contents) return
+
+        const depth = route.route.split('/').filter(Boolean).length - 1
+        const prefix = depth > 0 ? '../'.repeat(depth) : './'
+        // remove the trailing slash since we already have 
+        // a slash at the beginning of each path we want
+        // to modify.
+        // -- Omar I. Jul 13 2026
+        const p = prefix.slice(0, -1)
+
+        route.contents = route.contents.replace(
+          /\b(src|href)="(\/[^"]*)"/g,
+          (m, attr, val) => {
+            if (val.startsWith('//')) return m
+            if (attr === 'href') {
+              const [path, ...rest] = val.split(/([?#])/)
+              const qh = rest.join('')
+              const target = path === '/' ? `${prefix}index.html${qh}` : `${p}${path}.html${qh}`
+              return `${attr}="${target}"`
+            }
+            return `${attr}="${p}${val}"`
+          }
+        )
+      }
+    }
+  }
 };
 
 export default staticOverrides;
