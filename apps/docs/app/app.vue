dc3f7e85a91f8f0ca7c0b6f3b91fda6c1438c05f
Omar Ibrahim <omaribrahim@uchicago.edu>
2026-07-13 14:31:17 -0500

    Feat(docs): Bake CSS signal for offline mode to HTML tag

--- apps/docs/app/app.vue
+++ apps/docs/app/app.vue
@@ -6,3 +6,13 @@
     </NuxtErrorBoundary>
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const { public: { isOffline } } = useRuntimeConfig();
+
+useHead({
+  htmlAttrs: {
+    'data-mode': isOffline ? 'offline' : 'online',
+  },
+});
+</script>
